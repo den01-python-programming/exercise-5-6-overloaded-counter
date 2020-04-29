166eca1583bff34398788921f952a69072d6a721
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-29 10:35:28 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,6 +1,18 @@
 import pytest
-import src.exercise
+from src.counter import Counter
 
 def test_exercise():
-    #implement tests here
-    assert 0 == 0
+    counter = Counter(10)
+    assert counter.value() == 10
+
+    counter.increase(5)
+    assert counter.value() == 15
+
+    counter.decrease(10)
+    assert counter.value() == 5
+
+    counter.increase()
+    assert counter.value() == 6
+
+    counter.decrease()
+    assert counter.value() == 5
