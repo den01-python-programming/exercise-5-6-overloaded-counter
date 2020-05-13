440425db4c9c819be9d64adbb14530895b4bcb78
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-13 12:06:00 +0100

    Update test_exercise.py

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,16 +3,16 @@ from src.counter import Counter
 
 def test_exercise():
     counter = Counter(10)
-    assert counter.value() == 10
+    assert counter.value == 10
 
     counter.increase(5)
-    assert counter.value() == 15
+    assert counter.value == 15
 
     counter.decrease(10)
-    assert counter.value() == 5
+    assert counter.value == 5
 
     counter.increase()
-    assert counter.value() == 6
+    assert counter.value == 6
 
     counter.decrease()
-    assert counter.value() == 5
+    assert counter.value == 5
